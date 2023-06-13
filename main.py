cc09e92fb97ab67bb183eeae40c025f2da51af61
Bull1303 <2lp2@mail.ru>
2023-06-13 12:29:41 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,65 @@
+from pprint import pprint
+# Читаем адресную книгу в формате CSV в список contacts_list:
+import csv
+import re
+
+
+def merge_lists(list1, list2):
+    new_list = []
+    for i in range(len(list2)):
+        if list2[i] == '':
+            new_list.append(list1[i])
+        else:
+            new_list.append((list2[i]))
+    return new_list
+
+
+with open("phonebook_raw.csv", encoding="utf-8") as f:
+    rows = csv.reader(f, delimiter=",")
+    contacts_list = list(rows)
+
+
+# 1. Выполните пункты 1-3 задания.
+
+# 1. Поместить Фамилию, Имя и Отчество человека в поля lastname, firstname и surname соответственно.
+# В записной книжке изначально может быть Ф + ИО, ФИО, а может быть сразу правильно: Ф+И+О.
+
+for line in contacts_list:
+    FIO = ' '.join(line[0:3]).strip().split(' ')
+    FIO = FIO + [''] * (3 - len(FIO))
+    line[:3] = FIO[:3]
+
+    # 2. Привести все телефоны в формат +7(999)999-99-99.
+    # Если есть добавочный номер, формат будет такой: +7(999)999-99-99 доб.9999.
+
+    pattern = r"(\+7|8)\W*(\d{3})\W*(\d{3})[\s-]*(\d{2})[\s-]*(\d{2})"
+    subs = r"+7(\2)\3-\4-\5"
+    phones = re.sub(pattern, subs, line[5])
+    result_phones = re.sub(r"\(*доб.\s*(\d{4})\)*", r"доб.\1", phones)
+    line[5] = result_phones
+
+# 3. Объединить все дублирующиеся записи о человеке в одну.
+
+temp_list = []
+merge_list = []
+contacts_new = [contacts_list[0]]
+
+for id in range(1, len(contacts_list)):
+    line = contacts_list[id]
+    id_list = [n for n, x in enumerate(contacts_list) if x[:2] == line[:2]]
+    merge_list = [''] * len(line)
+    for id in id_list:
+        if id not in temp_list:
+            merge_list = merge_lists(contacts_list[id], merge_list)
+            temp_list.append(id)
+            print(merge_list)
+    if ''.join(merge_list) != '':
+        contacts_new.append(merge_list)
+pprint(contacts_new)
+
+
+# 2. Сохраните получившиеся данные в другой файл.
+# Код для записи файла в формате CSV:
+with open("phonebook.csv", "w", encoding="utf-8") as f:
+    datawriter = csv.writer(f, delimiter=',')
+    datawriter.writerows(contacts_new)
